98869f335a33994bde2a9046e8efeb5723ade722
xin-guo <56767485+XinGuo111@users.noreply.github.com>
2021-02-17 13:47:03 -0500

    2021-02-16 (#43)
    Cynthia Demo 1
    Login Page & Functions Page

---
+++ App/app/src/main/java/com/example/apiverte_colony_management/Functions.java
@@ -0,0 +1,24 @@
+package com.example.apiverte_colony_management;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Functions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_functions);
+
+        Button sign_in = findViewById(R.id.add_new_colony);
+        sign_in.setOnClickListener( new View.OnClickListener()
+        {   public void onClick(View v) {
+            Intent goToAddNewColony = new Intent(Functions.this, AddNewColony.class);
+            startActivity(goToAddNewColony);
+        } });
+    }
+}
--- App/app/src/main/java/com/example/apiverte_colony_management/MainActivity.java
+++ App/app/src/main/java/com/example/apiverte_colony_management/MainActivity.java
@@ -2,13 +2,45 @@ package com.example.apiverte_colony_management;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    String[] users = { "Oliver Smith", "Jack Williams", "Amelia Miller", "Harry Davis", "Emily Garcia" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spin = findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.spinner_item, users);
+        //adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin.setAdapter(adapter);
+        spin.setOnItemSelectedListener(this);
+
+        Button sign_in = findViewById(R.id.sign_in);
+        sign_in.setOnClickListener( new View.OnClickListener()
+        {   public void onClick(View v) {
+            Intent goToFunctions = new Intent(MainActivity.this, Functions.class);
+            startActivity(goToFunctions);
+        } });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
