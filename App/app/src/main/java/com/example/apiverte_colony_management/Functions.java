77730e305371639743c8bab04fdce2e3f4d9c8d7
alexiossu <boringalexi@gmail.com>
2021-03-17 02:23:22 -0400

    First commit. identify colony and typical inspection is ready

--- App/app/src/main/java/com/example/apiverte_colony_management/Functions.java
+++ App/app/src/main/java/com/example/apiverte_colony_management/Functions.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -15,10 +16,21 @@ public class Functions extends AppCompatActivity {
         setContentView(R.layout.activity_functions);
 
         Button sign_in = findViewById(R.id.add_new_colony);
+
         sign_in.setOnClickListener( new View.OnClickListener()
         {   public void onClick(View v) {
             Intent goToAddNewColony = new Intent(Functions.this, CreateColonyPage.class);
             startActivity(goToAddNewColony);
+
         } });
+
+        Button identifyColony = findViewById(R.id.typical_inspection);
+
+            Intent goToIdentifyColony = new Intent(Functions.this, IdentifyColony.class);
+            identifyColony.setOnClickListener(e -> {
+                startActivity(goToIdentifyColony);
+            });
+
+            Log.i("WRONG", "Something is wrong here");
+        }
     }
-}
